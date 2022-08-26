042fe0b1decdc9e76e3e543d985c031f0c9c56ae
Mark Caggiano <mcisback@gmail.com>
2022-08-27 00:18:39 +0200

    +cookies + listing + onebyone

--- scrapershell.js
+++ scrapershell.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 
 const fs = require('fs');
+const fsProm = fs.promises;
 const puppeteer = require('puppeteer');
 // const process = require('process')
 const args = process.argv.slice(2);
@@ -14,20 +15,45 @@ if(args.length <= 0) {
 
 const requestFile = args[0];
 
+const saveCookie = async (page, cookieFile = 'cookies.json') => {
+    const cookies = await page.cookies();
+    const cookieJson = JSON.stringify(cookies, null, 2);
+    await fsProm.writeFile(cookieFile, cookieJson);
+}
+
+//load cookie function
+const loadCookie = async (page, cookieFile = 'cookies.json') => {
+    const cookieJson = await fsProm.readFile(cookieFile);
+    const cookies = JSON.parse(cookieJson);
+    await page.setCookie(...cookies);
+}
+
 let bufferData = fs.readFileSync(requestFile);
 let stData = bufferData.toString();
 let {
     url,
     qs = null,
-    puppeteersOptions,
+    cookieFile = null,
+    saveCookies = false,
+    outputFile = null,
+    userAgent = null,
+    puppeteersOptions = {
+        headless: true,
+        ignoreHTTPSErrors: true
+    },
     config = {},
     nextPage = null,
+    preScrape = null,
     scrape
 } = JSON.parse(stData);
 
-let { parentSelector, columns } = scrape;
+let { parentSelector = null, columns, scrapeMode = 'listing' } = scrape;
 
-let { suppressBrowserLog = true, acceptCookiesSelector = null } = config;
+let {
+    suppressBrowserLog = true,
+    acceptCookiesSelector = null ,
+    browserLogOutputFile = null
+} = config;
 // let { url, puppettersOptions, columns } = config
 
 if(qs !== null) {
@@ -43,6 +69,19 @@ if(qs !== null) {
 // console.log('nextPage: ', nextPage)
 // process.exit(1)
 
+const timestamp = new Date().getTime()
+
+const logBrowserToFile = (fileName, message) => {
+    const logFilename = fileName.replace('{{timestamp}}', timestamp)
+
+/*    if(fs.existsSync(logFilename)) {
+        fs.appendFileSync(logFilename, message)
+    } else {
+        console.log(`Creating File ${logFilename}`)*/
+
+    fs.writeFileSync(logFilename, message)
+    // }
+}
 
 (async () => {
     // console.log('url: ', url)
@@ -62,6 +101,14 @@ if(qs !== null) {
     try {
         const page = await browser.newPage()
 
+        if(!!userAgent) {
+            await page.setUserAgent(userAgent)
+        }
+
+        if(!!cookieFile) {
+            await loadCookie(page, cookieFile)
+        }
+
         const {blue, cyan, green, magenta, red, yellow} = require('colorette')
         page
             .on('console', message => {
@@ -74,21 +121,32 @@ if(qs !== null) {
                 }
                 const color = colors[type] || blue
 
-                if(!suppressBrowserLog) {
-                    console.log(color(`[${type}] ${message.text()}`))
+                if(suppressBrowserLog === false) {
+                    if(!!browserLogOutputFile) {
+                        logBrowserToFile(browserLogOutputFile, `[${type}] ${message.text()}`)
+                    } else {
+                        console.log(color(`[${type}] ${message.text()}`))
+                    }
                 }
-                // console.log('')
             })
             .on('pageerror', ({message}) => {
-                if(!suppressBrowserLog) {
-                    console.log(red(message))
+                if(suppressBrowserLog === false) {
+                    if(!!browserLogOutputFile) {
+                        logBrowserToFile(browserLogOutputFile, `[ERROR] ${message.text()}`)
+                    } else {
+                        console.log(red(`[ERROR] ${message.text()}`))
+                    }
                 }
             })
             // .on('response', response =>
             //     console.log(green(`${response.status()} ${response.url()}`)))
             .on('requestfailed', request => {
-                if(!suppressBrowserLog) {
-                    console.log(magenta(`${request.failure().errorText} ${request.url()}`))
+                if(suppressBrowserLog === false) {
+                    if(!!browserLogOutputFile) {
+                        logBrowserToFile(browserLogOutputFile, `[REQUEST FAILED] ${request.failure().errorText} ${request.url()}`)
+                    } else {
+                        console.log(magenta(`[REQUEST FAILED] ${request.failure().errorText} ${request.url()}`))
+                    }
                 }
             })
 
@@ -99,6 +157,31 @@ if(qs !== null) {
 
             await page.click(acceptCookiesSelector)
         }
+
+        if(preScrape !== null) {
+            const { actions } = preScrape
+
+            for(let i = 0; i < actions.length; i++) {
+                const action = actions[i]
+
+                const { type, selector = null, config } = action
+
+                if(type === 'click') {
+                    await page.click(selector)
+                } else if(type === 'type') {
+                    await page.type(selector, config.value)
+                } else if(type === 'timeout') {
+                    await page.waitForTimeout(config.value);
+                } else if(type === 'waitVisibility') {
+                    await page.waitForSelector(selector, config.options)
+                    await page.waitForFunction(`document.querySelector('${selector}').clientHeight != 0`)
+                } else if(type === 'waitDisplay') {
+                    await page.waitForSelector(selector, config.options)
+                    await page.waitForFunction(`document.querySelector('${selector}').style.display != 'none'`)
+                }
+            }
+        }
+
 
         if(nextPage !== null && nextPage.active !== false) {
             // console.log('nextPage: ', JSON.stringify(nextPage))
@@ -124,9 +207,51 @@ if(qs !== null) {
             }
         }
 
-        const retrievedData = await page.evaluate((parentSelector, columns, nextPage) => {
+        const retrievedData = await page.evaluate((parentSelector, scrapeMode, columns, nextPage) => {
             // This block has the page context, which is almost identical to being in the console
             // except for some of the console's supplementary APIs.
+
+            const getColValue = (el, { type, config }) => {
+                let colValue = '';
+
+                if (type === 'text') {
+                    colValue = !!el ? el.textContent : ''
+                } else if (type === 'html') {
+                    colValue = !!el ? el.innerHTML : ''
+                } else if (type === 'attr') {
+                    colValue = !!el ? el.getAttribute(config.attr) : ''
+                } else if (type === 'exists') {
+                    console.log('exists ? ', !!el, el)
+
+                    return !!el
+                } else if (type === 'hasvalue') {
+                    // console.log('el: ', !!el)
+
+                    const isEl = !!el
+
+                    if (!isEl) {
+                        return false
+                    }
+
+                    colValue = getColValue(el, !!config.type ? config.type : 'text')
+
+                    // console.log('colValue: ', colValue)
+
+                    if (colValue === '') {
+                        return false
+                    } else {
+                        config.caseSensitive = !!config.caseSensitive ? config.caseSensitive : false
+                        colValue = config.caseSensitive === true ? colValue : colValue.toLowerCase()
+
+                        console.log('colValue === (config.caseSensitive ? config.value : config.value.toLowerCase()): ', colValue === (config.caseSensitive ? config.value : config.value.toLowerCase()))
+                        return (colValue === (config.caseSensitive ? config.value : config.value.toLowerCase()))
+                    }
+                } else {
+                    throw new Error(`${type} is not supported`)
+                }
+
+                return colValue
+            }
 
             // Get the URL host name and path separately
             const {origin, pathname, href} = window.location;
@@ -135,123 +260,101 @@ if(qs !== null) {
 
             const data = []
 
-            const parentElements = Array.from(document.querySelectorAll(parentSelector))
+            console.log('SCRAPE MODE: ', scrapeMode)
 
-            // console.log('parentElements:', parentElements.length)
+            if(scrapeMode === 'listing') {
+                if(parentSelector === null) {
+                    parentSelector = 'body'
+                }
 
-            for(let childIndex = 0; childIndex < parentElements.length; childIndex++) {
-                const parent = parentElements[ childIndex ];
-
-                // let currentElementIndex = 0;
+                const parentElement = document.querySelector(parentSelector)
 
                 const item = {}
+
+                const firstArray = Array.from(document.querySelectorAll(parentSelector))
+
+                let maxLength = firstArray.length
+                let maxIndex = 0
+
+                const tmpData = {}
 
                 for (let i = 0; i < keys.length; i++) {
                     const column = keys[i]
 
-                    // console.log('column: ', column)
+                    const elements = Array.from(document.querySelectorAll(
+                        columns[column].selector
+                    ))
 
-                    const {
-                        selector,
-                        type,
-                        config = {
-                            attr: null,
-                            type: null,
+                    if(maxLength < elements.length) {
+                        maxLength = elements.length
+                        maxIndex = i
+                    }
+                }
 
-                        }
-                    } = columns[column];
+                for(let i = 0; i < maxLength; i++) {
+                    const colData = {}
+                    for (let j = 0; j < keys.length; j++) {
+                        const column = keys[j]
 
-                    // console.log('columns[ column ]: ', selector, type, attr )
+                        const el = document.querySelectorAll(
+                            columns[column].selector
+                        )[i]
 
-                    const getColValue = (el, type) => {
-                        let colValue = '';
-
-                        if (type === 'text') {
-                            colValue = !!el ? el.textContent : ''
-                        } else if (type === 'html') {
-                            colValue = !!el ? el.innerHTML : ''
-                        } else if (type === 'attr') {
-                            colValue = !!el ? el.getAttribute(config.attr) : ''
-                        } else if (type === 'expectedValue'){
-                            console.log('el: ', !!el)
-
-                            if(!el) {
-                                return false
-                            }
-
-                            colValue = getColValue(el, !!config.type ? config.type : 'text')
-
-                            console.log('colValue: ', colValue)
-
-                            if(colValue === '') {
-                                return false
-                            } else {
-                                config.caseSensitive = !!config.caseSensitive ? config.caseSensitive : false
-                                colValue = config.caseSensitive === true ? colValue : colValue.toLowerCase()
-
-                                return colValue === (config.caseSensitive ? config.value : config.value.toLowerCase())
-                            }
-                        }
-
-                        return colValue
+                        colData[column] = getColValue(el, columns[column])
                     }
 
-                    let elements = parent.querySelectorAll(selector)
-                    // console.log('elements: ', elements.length)
+                    data.push(colData)
+                }
 
-                    if(elements.length === 0) {
-                        item[column] = ''
-                    } else if(elements.length === 1) {
-                        item[column] = getColValue(elements[0], type)
-                    } else {
-                        // TODO
-                        item[column] = Array.from(elements/*[ currentElementIndex++ ]*/)
-                            .map(el => getColValue(el, type))
+            } else if(scrapeMode === 'onebyone') {
+
+                const parentElements = Array.from(document.querySelectorAll(parentSelector))
+
+                // console.log('parentElements:', parentElements.length)
+
+                for (let childIndex = 0; childIndex < parentElements.length; childIndex++) {
+                    const parent = parentElements[childIndex];
+
+                    const item = {}
+
+                    for (let i = 0; i < keys.length; i++) {
+                        const column = keys[i]
+
+                        // console.log('column: ', column)
+
+                        const {
+                            selector,
+                            type,
+                            config = {
+                                attr: null,
+                                type: null,
+
+                            }
+                        } = columns[column];
+
+                        // console.log('columns[ column ]: ', selector, type, attr )
+
+                        let elements = parent.querySelectorAll(selector)
+                        // console.log('elements.length: ', elements.length)
+
+                        if (elements.length === 0) {
+                            item[column] = ''
+                        } else if (elements.length === 1) {
+                            console.log('elements[0] is null ? ', !!elements[0])
+                            
+                            item[column] = getColValue(elements[0], columns[column])
+                        } else {
+                            console.log('PASSA DI QUI')
+
+                            item[column] = Array.from(elements)
+                                .map(el => getColValue(el, columns[column]))
+                        }
+
                     }
 
-
-
-                    // console.log('tmpData[column]: ', tmpData[column])
-                }
-
-                data.push(item)
-            }
-
-            /*let maxLenght = tmpData[keys[0]].length;
-            let maxLenghtIndex = 0;
-
-            for (let colIndex = 0; colIndex < keys.length; colIndex++) {
-                console.log(keys[colIndex], tmpData[keys[colIndex]].length)
-
-                if (tmpData[keys[colIndex]].length > maxLenght) {
-                    maxLenght = tmpData[keys[colIndex]].length
-                    maxLenghtIndex = colIndex
+                    data.push(item)
                 }
             }
-
-            console.log('MAX LENGHT: ', maxLenght)
-            console.log('MAX LENGHT INDEX: ', maxLenghtIndex)
-
-            // console.log('PROCESSING DATA: ', tmpData[ keys[0] ].length)
-            // console.log('PROCESSING DATA: ', tmpData[ keys[1] ].length)
-            // console.log('PROCESSING DATA: ', tmpData[ keys[2] ].length)
-
-            const data = []
-
-            for (let dataIndex = 0; dataIndex < tmpData[ keys[maxLenghtIndex] ].length; dataIndex++) {
-                const colData = {}
-
-                for (let colIndex = 0; colIndex < keys.length; colIndex++) {
-                    const column = keys[colIndex]
-
-                    // console.log('column: ', column)
-                    // console.log('tmpData[column][dataIndex]: ', tmpData[column][dataIndex])
-
-                    colData[column] = !!tmpData[column][dataIndex] ? tmpData[column][dataIndex] : ''
-                }
-
-                data.push(colData)
-            }*/
 
             return {
                 origin,
@@ -260,16 +363,21 @@ if(qs !== null) {
                 length: data.length,
                 data,
             }
-        }, parentSelector, columns, nextPage)
+        }, parentSelector, scrapeMode, columns, nextPage)
 
-        // console.log(retrievedData)
-
-        // Convert the object from the browser eval to JSON to parse with with jq later
         const retrievedJSON = JSON.stringify(retrievedData, null, 4)
 
-        // console.log writes to stdout in node
-        console.log(retrievedJSON)
+        if(!!outputFile) {
+            console.log(`[OUTPUT] Writing ${outputFile}`)
 
+            fs.writeFileSync(outputFile, retrievedJSON)
+        } else {
+            console.log(retrievedJSON)
+        }
+
+        if(saveCookies === true) {
+            await saveCookie(page);
+        }
         await browser.close()
 
     } catch(e) {
